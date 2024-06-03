e2eb352aebfde36161638b853a5c72bde0fcff1f
eeshvardasikcm-workflow <eeshvars-google-cloud-innovator-acct@eeshvardasikcm.com>
2024-06-02 18:58:50 -0700

    Create a-beginning-point.h

---
+++ eeshvardasikcm/a-beginning-point.h
@@ -0,0 +1,48 @@
+/*
+Copyright 2024 EeshvarDasIKCM (Erik Douglas Ward, eeshvardasikcm)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// https://github.com/eeshvardasikcm/a-beginning-point
+
+namespace a-beginning-point {
+  class Stability {
+    static int ROBUST_CRITICAL = 0;
+    static int ROBUST_MODULE_1 = 1;
+    static int ROBUST_MODULE_2 = 2;
+    static int ROBUST_MODULE_3 = 3;
+    static int ROBUST_MODULE_4 = 4;
+    static int ROBUST_MODULE_5 = 5;
+    static int ROBUST_MODULE_6 = 6;
+    static int ROBUST_MODULE_7 = 7;
+    static int ROBUST_MODULE_8 = 8;
+    static int ROBUST_MODULE_9 = 9;
+    static int ROBUST_INSTABILITY = 10;
+    static int CRITICAL_MODULE_1 = 11;
+    static int CRITICAL_MODULE_2 = 12;
+    static int CRITICAL_MODULE_3 = 13;
+  };
+  class Instability {
+    static int INSTABILITY_ZERO = 0;
+    static int INSTABILITY_MODULE_1 = 1;
+    static int INSTABILITY_MODULE_2 = 2;
+    static int INSTABILITY_MODULE_3 = 3;
+    static int INSTABILITY_MODULE_4 = 4;
+    static int INSTABILITY_MODULE_5 = 5;
+    static int INSTABILITY_MODULE_6 = 6;
+    static int INSTABILITY_MODULE_7 = 7;
+    static int INSTABILITY_MODULE_8 = 8;
+    static int INSTABILITY_MODULE_9 = 9;
+  };
+}
